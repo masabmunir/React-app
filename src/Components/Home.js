04d772d640ed1ab1c67419e565c5473b4d9b4f38
Masab Munir <masabmunir>
2023-08-25 04:36:23 +0500

    api call in react

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,31 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Blogdata from './Blogdata';
 
 const Home =()=>{
-    const [blog,setblog] = useState(
-        [
-            {Name:"Masab", Age:12, Gender:"Male",id:1},
-            {Name:"Ali", Age:13, Gender:"Male",id:2},
-            {Name:"Hamza", Age:15, Gender:"Male",id:3}
+    const [blog,setblog] = useState()
+        // [
+        //     {Name:"Masab", Age:12, Gender:"Male",id:1},
+        //     {Name:"Ali", Age:13, Gender:"Male",id:2},
+        //     {Name:"Hamza", Age:15, Gender:"Male",id:3}
 
-        ]);
+        // ]);
 
     const handleDelete=(id)=>{
         const blogdata = blog.filter(blogs=>blogs.id !==id)
         setblog(blogdata)
-    }    
+    }
+    
+    useEffect(()=>{
+        fetch("http://localhost:3000/Employees").then(res=>{
+            return res.json;
+        }).then(data=>{
+            setblog(data);
+        })
+    })
     return(
         <div className="home">
             <h3>HomePage</h3>
-            <Blogdata blogs={blog} title="My Blogs" handleDelete={handleDelete} />
+            {blog && <Blogdata blogs={blog} title="My Blogs" handleDelete={handleDelete} />}
         </div>
     );
 }
