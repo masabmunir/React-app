e682b4e85351b860222db3a8a27b910e121c194d
Masab Munir <masabmunir>
2023-08-24 20:36:43 +0500

    adding some components

--- src/Components/Blogdata.js
+++ src/Components/Blogdata.js
@@ -1,10 +1,17 @@
-const Blogdata = (props) => {
-    const blogs = props.blogs
-    console.log(blogs)
+import React from 'react';
+
+const Blogdata = ({blogs,title,handleDelete}) => {
 
     return (
         <div>
-       
+            <h3>{title}</h3>
+            {blogs?.map((blog) => (
+                <div key={blog.id}>
+                    <p>{blog.Name}</p>
+                    <p>{blog.Gender}</p>
+                    <button onClick={()=>{handleDelete(blog.id)}}>Delete data</button>
+                </div>
+            ))}
         </div>
     );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,29 +1,25 @@
-import {useState} from "react";
-import Blogdata from "./Blogdata";
-const Home =()=>{
+import {useState} from 'react';
+import Blogdata from './Blogdata';
 
+const Home =()=>{
     const [blog,setblog] = useState(
         [
             {Name:"Masab", Age:12, Gender:"Male",id:1},
             {Name:"Ali", Age:13, Gender:"Male",id:2},
             {Name:"Hamza", Age:15, Gender:"Male",id:3}
 
-        ]
-    );
+        ]);
 
+    const handleDelete=(id)=>{
+        const blogdata = blog.filter(blogs=>blogs.id !==id)
+        setblog(blogdata)
+    }    
     return(
         <div className="home">
             <h3>HomePage</h3>
-           
-           <Blogdata blog={blog} />
-                {/* {blog.map((info)=>(
-                    <div key={info.id}>
-                      <p>{info.Name}</p>
-                      <p>{info.Gender}</p>
-                    </div>
-                ))} */}
+            <Blogdata blogs={blog} title="My Blogs" handleDelete={handleDelete} />
         </div>
     );
 }
 
-export default Home
+export default Home;
